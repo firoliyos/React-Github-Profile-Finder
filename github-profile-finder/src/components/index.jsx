e15570851227aa9f0a762b8de91e635a5082a619
firoliyos <firatesfaye44@gmail.com>
2024-10-30 16:06:56 +0300

    api-fetch

--- github-profile-finder/src/components/index.jsx
+++ github-profile-finder/src/components/index.jsx
@@ -1,14 +1,31 @@
-
+import {useState, useEffect} from 'react'
 
 export default function GithubProfileFinder() {
+
+    const [userName, setUserName] = useState('firoliyos')
+    async function fetchGithubUserData() {
+        const res = await fetch(`https://api.github.com/users/${userName}`)
+        const data = await res.json()
+
+        console.log(data)
+    }
+    function handleSubmit() {
+        
+    }
+
+    useEffect(()=> {
+      fetchGithubUserData()
+    }, [])
     return (
         <div className="input-wrapper">
           <input 
           name="search-by-userName"
           type="text"
           placeholder="Search Github Username..."
+          value={userName}
+          onChange={(event)=> setUserName(event.target.value)}
           />  
-          <button>Search</button>
+          <button onClick={handleSubmit}>Search</button>
         </div>
     )
 }
