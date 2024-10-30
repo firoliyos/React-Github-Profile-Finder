bf4c6aa178793255d5b251023723c3c8e5f647e0
firoliyos <firatesfaye44@gmail.com>
2024-10-30 16:12:30 +0300

    setLoading-added

--- github-profile-finder/src/components/index.jsx
+++ github-profile-finder/src/components/index.jsx
@@ -3,11 +3,19 @@ import {useState, useEffect} from 'react'
 export default function GithubProfileFinder() {
 
     const [userName, setUserName] = useState('firoliyos')
+    const [userData, setUserData] = useState(null)
+    const [loading, setLoading] = useState(false)
     async function fetchGithubUserData() {
+        setLoading(true)
         const res = await fetch(`https://api.github.com/users/${userName}`)
         const data = await res.json()
 
+        if(data) {
+            setUserData(data)
+            setLoading(false)
+        }
         console.log(data)
+       
     }
     function handleSubmit() {
         
@@ -16,6 +24,12 @@ export default function GithubProfileFinder() {
     useEffect(()=> {
       fetchGithubUserData()
     }, [])
+
+    if(loading) {
+        return (
+            <h1>LOADING...</h1>
+        )
+    }
     return (
         <div className="input-wrapper">
           <input 
