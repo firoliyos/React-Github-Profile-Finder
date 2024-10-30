67d6105e247e20fd4378903707d9e67552070c13
firoliyos <firatesfaye44@gmail.com>
2024-10-30 19:56:10 +0300

    index.jsx-file

--- github-profile-finder/src/components/index.jsx
+++ github-profile-finder/src/components/index.jsx
@@ -1,51 +1,49 @@
-import User from './user'
-import {useState, useEffect} from 'react'
-import './styles.css'
+import { useEffect, useState } from "react";
+import User from "./user";
+import './styles.css';
+
 export default function GithubProfileFinder() {
+  const [userName, setUserName] = useState("sangammukherjee");
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const [userName, setUserName] = useState('firoliyos')
-    const [userData, setUserData] = useState(null)
-    const [loading, setLoading] = useState(false)
-    async function fetchGithubUserData() {
-        setLoading(true)
-        const res = await fetch(`https://api.github.com/users/${userName}`)
-        const data = await res.json()
+  async function fetchGithubUserData() {
+    setLoading(true);
+    const res = await fetch(`https://api.github.com/users/${userName}`);
 
-        if(data) {
-            setUserData(data)
-            setLoading(false)
-            setUserName('')
-        }
+    const data = await res.json();
+    if (data) {
+      setUserData(data);
+      setLoading(false);
+      setUserName('')
     }
-    function handleSubmit() {
-        fetchGithubUserData()
-    }
+  }
 
-    useEffect(()=> {
-      fetchGithubUserData()
-    }, [])
+  function handleSubmit() {
+    fetchGithubUserData()
+  }
 
-    if(loading) {
-        return (
-            <h1>LOADING...</h1>
-        )
-    }
-    return (
-        <div className="github-profile-container">
-          <div className="input-wrapper">
-           <input 
-            name="search-by-userName"
-            type="text"
-            placeholder="Search Github Username..."
-            value={userName}
-            onChange={(event)=> setUserName(event.target.value)}
-           />  
-          <button onClick={handleSubmit}>Search</button>
-          </div>
-          {
-            userData !== null ? <User user={userData}/>: null
-          }
-        </div>
-        
-    )
+  useEffect(() => {
+    fetchGithubUserData();
+  }, []);
+
+  if (loading) {
+    return <h1>Loading data ! Please wait</h1>;
+  }
+
+  return (
+    <div className="github-profile-container">
+      <div className="input-wrapper">
+        <input
+          name="search-by-username"
+          type="text"
+          placeholder="Search Github Username..."
+          value={userName}
+          onChange={(event) => setUserName(event.target.value)}
+        />
+        <button onClick={handleSubmit}>Search</button>
+      </div>
+      {userData !== null ? <User user={userData} /> : null}
+    </div>
+  );
 }
